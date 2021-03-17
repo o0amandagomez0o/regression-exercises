ccb58999829d6cbc028b0563fbe78c2f2cb35ae9
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-03-16 23:05:43 -0500

    completed mpg set

--- evaluate.py
+++ evaluate.py
@@ -35,19 +35,119 @@ def plot_residuals(target, yhat):
     
     
     
-MSE = mean_squared_error(tips.tip, tips.yhat)
-SSE = MSE * len(tips)
-RMSE = sqrt(MSE)
-TSS = (mean_squared_error(tips.tip, tips.baseline)) * len ()
-ESS = TSS - SSE
+def regression_errors(target, yhat):
+    '''
+    regression_errors takes in a target and prediction series
+    and prints out the regression error metrics.
+    '''
+    residual = target - yhat
+    
+    mse = mean_squared_error(target, yhat)
+    sse = (residual **2).sum()
+    rmse = sqrt(mse)
+    tss = ((target - yhat.mean()) ** 2).sum()
+    ess = ((yhat - target.mean()) ** 2).sum()
+    print(f"""
+    MSE: {round(mse,2)}
+    SSE: {round(sse,2)}
+    RMSE: {round(rmse,2)}
+    TSS: {round(tss,2)}
+    ESS: {round(ess,2)}
+    """)
+    
+    
+    
+    
+    
+def baseline_mean_errors(target):
+    '''
+    baseline_mean_errors takes in a target 
+    and prints out the regression error metrics for the baseline.
+    '''
+    baseline = target.mean()
+    
+    residual = target - (baseline)
+    
+    sse_baseline = (residual **2).sum()
+    mse_baseline = sse_baseline * len(target)
+    rmse_baseline = sqrt(mse_baseline)
+    
+    print(f"""
+    MSE_baseline: {round(MSE_baseline,2)}
+    SSE_baseline: {round(SSE_baseline,2)}
+    RMSE_baseline: {round(RMSE_baseline,2)}
+    """)
+    
+    
+    
+    
+def better_than_baseline(target, yhat):
+    '''
+    better_than_baseline takes in a target and prediction 
+    and returns boolean answering if the model is better than the baseline.
+    '''
+    
+    rmse_baseline = sqrt((((target - (target.mean())) **2).sum()) * len(target))
+    rmse_model = sqrt((((target - yhat) **2).sum()) * len(target))
+    return rmse_model < rmse_baseline
 
 
-MSE_baseline = mean_squared_error(tips.tip, tips.baseline)
-SSE_baseline = MSE_baseline * len(tips)
-RMSE_baseline = sqrt(MSE_baseline)
+
+
+def model_significance(ols_model):
+    return {
+        'r^2 -- variance explained': ols_model.rsquared,
+        'p-value -- P(data|model == baseline)': ols_model.f_pvalue,
+    }
+
+
+
+
+def residuals(actual, predicted):
+    return actual - predicted
+
+def sse(actual, predicted):
+    return (residuals(actual, predicted) **2).sum()
+
+def mse(actual, predicted):
+    n = actual.shape[0]
+    return sse(actual, predicted) / n
+
+def rmse(actual, predicted):
+    return sqrt(mse(actual, predicted))
+
+def ess(actual, predicted):
+    return ((predicted - actual.mean()) ** 2).sum()
+
+def tss(actual):
+    return ((actual - actual.mean()) ** 2).sum()
 
 
 
 
 
-R2 = 1 - (SSE/TSS)
+def reg_error_metrics(target, yhat):
+    '''
+    reg_error_metrics takes in target and prediction series 
+    and returns a dataframe that contains the SSE/MSE/RMSE metrics 
+    for. both model and baseline
+    and answers if the model is better than the baseline.
+    '''
+    
+    df = pd.DataFrame(np.array(['SSE', 'MSE','RMSE']), columns=['metric'])
+    
+    df['model_error'] = np.array([sse(target, yhat),  mse(target, yhat), rmse(target, yhat)])
+    
+    df['baseline_error'] = np.array([sse(target, target.mean()), mse(target, target.mean()), rmse(target, target.mean())])
+    
+    df['better_than_baseline'] = df.baseline_error > df.model_error
+    
+    df = df.set_index("metric")
+    
+    return df
+    
+    
+    
+    
+    
+
